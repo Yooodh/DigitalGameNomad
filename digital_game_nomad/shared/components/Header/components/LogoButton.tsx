0cd07b180a7f0aa761e04d802340db9bd8f47577
Yooodh <eogus2604@hanmail.net>
2025-06-12 02:59:17 +0900

    Feat: 로고 이미지 및 메뉴 토글 LogoButton 컴포넌트 추가

---
+++ digital_game_nomad/shared/components/Header/components/LogoButton.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+// package
+import Link from 'next/link';
+import Image from 'next/image';
+
+// slice
+import styles from '../styles/Header.module.scss';
+import { LogoButtonProps } from '../types';
+
+export default function LogoButton({
+  onMenuItemClick,
+  onToggleMenu,
+  isMenuOpen,
+  toggleButtonRef,
+}: LogoButtonProps) {
+  return (
+    <div className={styles.headerContainer__logo}>
+      <Link href='/' onClick={onMenuItemClick}>
+        <Image
+          src='/images/half_logo_white.png'
+          alt='logo'
+          width={40}
+          height={40}
+        />
+      </Link>
+      <button
+        ref={toggleButtonRef}
+        onClick={onToggleMenu}
+        className={styles.headerContainer__toggleLogo}
+        aria-label={isMenuOpen ? '메뉴 닫기' : '메뉴 열기'}
+        aria-expanded={isMenuOpen}
+      >
+        <Image src='/images/hamburger.png' alt='menu' width={40} height={40} />
+      </button>
+    </div>
+  );
+}

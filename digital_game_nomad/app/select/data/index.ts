e433939812ae709d94b489c01d82ab811ad12455
Yooodh <eogus2604@hanmail.net>
2025-06-14 00:48:24 +0900

    Feat: 캐릭터 선택 페이지용 캐릭터 임시 데이터 추가

---
+++ digital_game_nomad/app/select/data/index.ts
@@ -0,0 +1,16 @@
+export const characters = [
+  {
+    id: 'aj',
+    name: 'AJ',
+    description: '사람들과 어울리며 전시를 활기차게 체험하는 타입',
+    image: '/images/aj.png',
+    color: '#ffffff',
+  },
+  {
+    id: 'stefani',
+    name: 'STEFANI',
+    description: '조용히 관찰하며 깊이 있게 감상하는 타입',
+    image: '/images/stefani.png',
+    color: '#ffffff',
+  },
+];

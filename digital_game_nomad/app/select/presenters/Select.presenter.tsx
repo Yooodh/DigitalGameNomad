b672120882e69481bdcdc6be28da9f787c87f6a9
Yooodh <eogus2604@hanmail.net>
2025-06-14 00:44:50 +0900

    Feat: SelectPresenter 컴포넌트 추가

---
+++ digital_game_nomad/app/select/presenters/Select.presenter.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+// package
+import { RefObject } from 'react';
+
+// slice
+import styles from '../styles/Select.module.scss';
+import SelectHeader from '../components/SelectHeader';
+import CharacterList from '../components/CharacterList';
+import { SelectPresenterProps } from '../types';
+
+// layer
+import ConditionalButton from '@/shared/components/ConditionalButton';
+
+export default function SelectPresenter({
+  characters,
+  selectedCharacter,
+  onCharacterSelect,
+  onEnterExhibition,
+  listRef,
+  enterButtonRef,
+}: SelectPresenterProps & {
+  enterButtonRef: RefObject<HTMLButtonElement | null>;
+}) {
+  return (
+    <div className={styles.selectContainer}>
+      <div className={styles.selectContainer__content}>
+        <SelectHeader />
+        <CharacterList
+          listRef={listRef}
+          characters={characters}
+          selectedCharacter={selectedCharacter}
+          onCharacterSelect={onCharacterSelect}
+        />
+        <div className={styles.selectContainer__loadingInfo}>
+          <p>※모바일 환경에서는 지원하지 않습니다.</p>
+        </div>
+        <ConditionalButton
+          buttonRef={enterButtonRef}
+          onClick={onEnterExhibition}
+          disabled={!selectedCharacter}
+          className={selectedCharacter ? styles.buttonActive : ''}
+        >
+          {selectedCharacter ? '전시관 입장하기' : '캐릭터를 선택해주세요'}
+        </ConditionalButton>
+      </div>
+    </div>
+  );
+}

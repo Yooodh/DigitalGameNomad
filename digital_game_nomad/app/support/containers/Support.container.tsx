81cb8748165c7803722118c4d8c9df5566152d3e
Yooodh <eogus2604@hanmail.net>
2025-06-17 00:14:34 +0900

    Feat: 고객센터 컨테이너 및 Faq 상태 관리 로직 구현

---
+++ digital_game_nomad/app/support/containers/Support.container.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+// package
+import { useState } from 'react';
+
+// slice
+import SupportPresenter from '../presenters/Support.presenter';
+import { UserState } from '../types';
+import { faqItems } from '../data';
+
+export default function SupportContainer() {
+  const [openFaq, setOpenFaq] = useState<number | null>(null);
+
+  const user: UserState = {
+    loginUserKey: undefined,
+    loginUserGrade: undefined,
+  };
+
+  const handleInquiryLinkClick = (
+    event: React.MouseEvent<HTMLAnchorElement>
+  ) => {
+    if (user.loginUserKey === undefined) {
+      // event.preventDefault();
+      // alert('로그인이 필요한 서비스입니다.');
+    }
+  };
+
+  const toggleFaq = (id: number) => {
+    setOpenFaq(openFaq === id ? null : id);
+  };
+
+  return (
+    <SupportPresenter
+      faqItems={faqItems}
+      openFaq={openFaq}
+      handleInquiryLinkClick={handleInquiryLinkClick}
+      toggleFaq={toggleFaq}
+    />
+  );
+}

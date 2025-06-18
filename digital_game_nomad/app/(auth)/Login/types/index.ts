674166a0388e502a1cc03df2805a00a550b300bc
Yooodh <eogus2604@hanmail.net>
2025-06-19 02:15:24 +0900

    Feat: 로그인 페이지 타입 추가

---
+++ digital_game_nomad/app/(auth)/Login/types/index.ts
@@ -0,0 +1,40 @@
+export type FormInputProps = {
+  icon: React.ElementType;
+  type: string;
+  name: string;
+  placeholder: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  errorMessage?: string;
+  children?: React.ReactNode;
+};
+
+export type LoginSubmitButtonProps = {
+  isLoading: boolean;
+};
+
+export type FormData = {
+  email: string;
+  password: string;
+};
+
+export type FormErrors = {
+  email?: string;
+  password?: string;
+};
+
+export type LoginPresenterProps = {
+  formData: {
+    email: string;
+    password: string;
+  };
+  errors: {
+    email?: string;
+    password?: string;
+  };
+  showPassword: boolean;
+  isLoading: boolean;
+  handleInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleSubmit: (e: React.FormEvent) => void;
+  togglePasswordVisibility: () => void;
+};

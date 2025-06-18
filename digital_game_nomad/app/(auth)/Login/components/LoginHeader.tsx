f055ef12a9463580443785aed1969f7d2469c235
Yo_o. <eogus2604@hanmail.net>
2025-06-19 02:22:53 +0900

    Feat: 로그인 페이지 UI 구현 (#10)
    * Feat: 아이콘, 에러 메시지 등을 지원하는 입력 컴포넌트 추가
    
    * Feat: 로그인 페이지 하단 회원가입 링크가 포함된 푸터 컴포넌트 추가
    
    * Feat: 로그인 페이지 상단 헤더 컴포넌트 추가
    
    * Feat: 로그인 상태 유지 체크박스 및 비밀번호 찾기 링크가 포함된 옵션 컴포넌트 추가
    
    * Feat: 로딩 상태를 지원하는 로그인 제출 버튼 컴포넌트 추가
    
    * Feat: 로그인 폼 상태 관리 및 유효성 검사 로직이 포함된 컨테이너 추가
    
    * Feat: 로그인 폼 레이아웃 및 상호작용을 담당하는 프레젠터 컴포넌트 추가
    
    * Style: 로그인 페이지 스타일 추가
    
    * Feat: 로그인 페이지 타입 추가
    
    * Feat: 로그인 페이지 추가

---
+++ digital_game_nomad/app/(auth)/Login/components/LoginHeader.tsx
@@ -0,0 +1,11 @@
+// slice
+import styles from '../styles/Login.module.scss';
+
+export default function LoginHeader() {
+  return (
+    <div className={styles.headerContainer}>
+      <h1 className={styles.headerContainer__title}>환영합니다</h1>
+      <p className={styles.headerContainer__subtitle}>계정에 로그인하세요</p>
+    </div>
+  );
+}
--- digital_game_nomad/app/(auth)/Login/page.tsx
+++ digital_game_nomad/app/(auth)/Login/page.tsx
@@ -1,5 +1,10 @@
-import React from 'react';
+// slice
+import LoginContainer from './containers/Login.container';
 
 export default function page() {
-  return <div>page</div>;
+  return (
+    <div>
+      <LoginContainer />
+    </div>
+  );
 }

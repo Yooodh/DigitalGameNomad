8d8d233d199ffbbd99b2090244186865844a42fe
Yooodh <eogus2604@hanmail.net>
2025-06-21 00:01:18 +0900

    Refactor: 클릭 및 라우팅 로직을 컨테이너로 분리

---
+++ digital_game_nomad/app/(main)/containers/Main.container.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+// package
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+// slice
+import MainPresenter from '../presenters/Main.presenter';
+
+export default function MainContainer() {
+  const [isClicked, setIsClicked] = useState<boolean>(false);
+  const router = useRouter();
+
+  const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setIsClicked(true);
+
+    setTimeout(() => {
+      router.push('/select');
+    }, 1200);
+  };
+
+  return <MainPresenter isClicked={isClicked} onClick={handleClick} />;
+}

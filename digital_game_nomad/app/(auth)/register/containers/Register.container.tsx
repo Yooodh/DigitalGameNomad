7fe24a26f7c64acf9a867aa32b9363370d71c247
Yooodh <eogus2604@hanmail.net>
2025-06-20 09:10:04 +0900

    Feat: 회원가입 로직과 프레젠터 연결 컨테이너 추가

---
+++ digital_game_nomad/app/(auth)/register/containers/Register.container.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+// slice
+import RegisterPresenter from '../presenters/Register.presenter';
+import { useRegisterForm } from '../hooks/useRegisterForm';
+
+export default function RegisterContainer() {
+  const {
+    formData,
+    validation,
+    showPassword,
+    showPasswordCheck,
+    verificationCode,
+    showVerification,
+    isLoading,
+    emailDomain,
+    isSelectOpen,
+    handleInputChange,
+    handleEmailDomainChange,
+    handleCarrierChange,
+    checkEmailDuplicate,
+    requestVerification,
+    verifyPhone,
+    handleSubmit,
+    setShowPassword,
+    setShowPasswordCheck,
+    setVerificationCode,
+    setIsSelectOpen,
+  } = useRegisterForm();
+
+  return (
+    <RegisterPresenter
+      formData={formData}
+      validation={validation}
+      showPassword={showPassword}
+      showPasswordCheck={showPasswordCheck}
+      verificationCode={verificationCode}
+      showVerification={showVerification}
+      isLoading={isLoading}
+      emailDomain={emailDomain}
+      isSelectOpen={isSelectOpen}
+      handleInputChange={handleInputChange}
+      handleEmailDomainChange={handleEmailDomainChange}
+      handleCarrierChange={handleCarrierChange}
+      checkEmailDuplicate={checkEmailDuplicate}
+      requestVerification={requestVerification}
+      verifyPhone={verifyPhone}
+      handleSubmit={handleSubmit}
+      setShowPassword={setShowPassword}
+      setShowPasswordCheck={setShowPasswordCheck}
+      setVerificationCode={setVerificationCode}
+      setIsSelectOpen={setIsSelectOpen}
+    />
+  );
+}

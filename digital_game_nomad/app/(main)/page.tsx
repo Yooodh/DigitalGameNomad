6900d8dc788422279c5d8a204fa0c5faaf64d109
Yo_o. <eogus2604@hanmail.net>
2025-06-24 01:38:47 +0900

    Refactor: 메인 페이지 컨테이너/프레젠터 구조로 분리 (#13)
    * Refactor: 클릭 및 라우팅 로직을 컨테이너로 분리
    
    * Refactor: 메인 페이지 UI를 프레젠터로 분리
    
    * Refactor: 메인 페이지 스타일 분리 및 애니메이션 추가
    
    * Refactor: 메인 페이지 타입 정의
    
    * Refactor: 메인 페이지에서 컨테이너 렌더링

--- digital_game_nomad/app/(main)/page.tsx
+++ digital_game_nomad/app/(main)/page.tsx
@@ -1,29 +1,10 @@
-// package
-import Image from 'next/image';
-import Link from 'next/link';
-
 // slice
-import styles from './main.module.scss';
+import MainContainer from './containers/Main.container';
 
-export default function Home() {
+export default function page() {
   return (
-    <div className={styles.homeContainer}>
-      <div className={styles.homeContainer__bg}>
-        <video className={styles.homeContainer__video} autoPlay muted loop>
-          <source src='/videos/network.mp4' type='video/mp4' />
-        </video>
-        <Link href='/select'>
-          <div className={styles.homeContainer__center}>
-            <Image
-              src='/images/full_logo_white.png'
-              alt='logo'
-              width={300}
-              height={300}
-            />
-            <button className={styles.homeContainer__btn}>Click to Play</button>
-          </div>
-        </Link>
-      </div>
+    <div>
+      <MainContainer />
     </div>
   );
 }

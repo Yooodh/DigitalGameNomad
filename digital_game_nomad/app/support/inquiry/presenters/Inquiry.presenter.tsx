d2c0ff9a61dc8c332b9074f1189aae679cb34ecc
Yooodh <eogus2604@hanmail.net>
2025-06-26 03:58:46 +0900

    Feat: 문의 페이지 프레젠터 컴포넌트 구현

---
+++ digital_game_nomad/app/support/inquiry/presenters/Inquiry.presenter.tsx
@@ -0,0 +1,61 @@
+// components/InquiryPresenter.tsx
+import styles from '../styles/Inquiry.module.scss';
+import FormInput from '../components/FormInput'; // Import the new components
+import FormTextarea from '../components/FormTextarea';
+import SubmitButton from '../components/SubmitButton';
+import Header from '../components/Header';
+import { InquiryPresenterProps } from '../types';
+
+export default function InquiryPresenter({
+  formData,
+  errors,
+  isSubmitting,
+  handleInputChange,
+  submitQuestion,
+}: InquiryPresenterProps) {
+  return (
+    <div className={styles.inquiryContainer}>
+      <Header
+        title='문의하기'
+        subTitle='궁금한 점이 있으시면 언제든 문의해 주세요'
+      />
+
+      <div className={styles.cardContainer}>
+        <form
+          className={styles.formContainer}
+          onSubmit={(e) => e.preventDefault()}
+        >
+          <FormInput
+            id='title'
+            label='제목'
+            placeholder='제목을 입력해주세요'
+            value={formData.title}
+            onChange={(value) => handleInputChange('title', value)}
+            error={errors.title}
+            disabled={isSubmitting}
+            required
+          />
+
+          <FormTextarea
+            id='content'
+            label='내용'
+            placeholder='문의 내용을 입력해주세요'
+            value={formData.text}
+            onChange={(value) => handleInputChange('text', value)}
+            error={errors.text}
+            disabled={isSubmitting}
+            required
+            rows={8}
+          />
+
+          <SubmitButton
+            isSubmitting={isSubmitting}
+            onClick={submitQuestion}
+            label='문의 등록'
+            submittingLabel='등록 중'
+          />
+        </form>
+      </div>
+    </div>
+  );
+}

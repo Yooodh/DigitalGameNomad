68b8409e69478e5ccc09e5b8271a5b9a90d8586d
Yooodh <eogus2604@hanmail.net>
2025-06-27 03:52:14 +0900

    Feat: 신청서 제출 후 안내 문구를 표시하는 푸터 컴포넌트 추가

---
+++ digital_game_nomad/app/apply/components/Footer.tsx
@@ -0,0 +1,10 @@
+// slice
+import styles from '../styles/Apply.module.scss';
+
+export default function Footer() {
+  return (
+    <div className={styles.footerContainer}>
+      <p>신청서 제출 후 검토를 거쳐 승인 여부를 안내드립니다.</p>
+    </div>
+  );
+}

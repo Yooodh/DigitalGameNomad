dafe1394a09f93d52175be6c70cbe2100730409e
Yo_o. <eogus2604@hanmail.net>
2025-06-27 04:03:31 +0900

    Feat: 기업 참여 신청서 작성 페이지 UI 구현 (#18)
    * Feat: 신청서 제출 후 안내 문구를 표시하는 푸터 컴포넌트 추가
    
    * Feat: 참여신청 페이지의 타이틀을 표시하는 헤더 컴포넌트 추가
    
    * Feat: 이미지 업로드 및 드래그앤드롭, 미리보기, 삭제 기능을 포함하는 컴포넌트 추가
    
    * Feat: 라벨, 아이콘, 텍스트 입력을 지원하는 범용 컴포넌트 추가
    
    * Feat: 신청서 제출 버튼 컴포넌트 추가
    
    * Feat: 게임 및 유튜브 URL 입력 필드를 포함하는 컴포넌트 추가
    
    * Feat: 참여 신청서 작성 페이지 프레젠터 컴포넌트 구현
    
    * Style: 참여 신청서 작성 페이지 스타일 추가

---
+++ digital_game_nomad/app/apply/components/Header.tsx
@@ -0,0 +1,13 @@
+// slice
+import styles from '../styles/Apply.module.scss';
+
+export default function Header() {
+  return (
+    <div className={styles.headerContainer}>
+      <h1 className={styles.headerContainer__title}>참여신청</h1>
+      <p className={styles.headerContainer__subTitle}>
+        게임 전시회에 참여하기 위한 정보를 입력해주세요
+      </p>
+    </div>
+  );
+}

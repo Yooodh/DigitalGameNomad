3b676eeb2374c028c71deb8a9f810f9e1e37f1b4
Yooodh <eogus2604@hanmail.net>
2025-07-03 02:01:45 +0900

    Feat: 공통 비밀번호 입력 필드 컴포넌트 추가

---
+++ digital_game_nomad/app/(auth)/reset-password/components/InputField.tsx
@@ -0,0 +1,48 @@
+// slice
+import styles from '../styles/ResetPassword.module.scss';
+import { InputFieldProps } from '../types';
+
+// layer
+import PasswordToggleButton from '@/shared/components/PasswordToggleButton/PasswordToggleButton';
+import { Warning } from '@/shared/icons';
+
+export default function InputField({
+  label,
+  name,
+  value,
+  showPassword,
+  onChange,
+  onTogglePassword,
+  placeholder,
+  error,
+}: InputFieldProps) {
+  return (
+    <div className={styles.inputContainer}>
+      <label className={styles.inputContainer__label}>{label}</label>
+      <div className={styles.inputContainer__wrap}>
+        <input
+          type={showPassword ? 'text' : 'password'}
+          name={name}
+          value={value}
+          onChange={onChange}
+          placeholder={placeholder}
+          className={`${styles.inputContainer__input} ${
+            error ? styles.inputContainer__error : ''
+          }`}
+        />
+        <PasswordToggleButton
+          showPassword={showPassword}
+          togglePasswordVisibility={onTogglePassword}
+        />
+      </div>
+      {error && (
+        <div className={styles.inputContainer__errorMessage}>
+          <svg className={styles.inputContainer__errorIcon} viewBox='0 0 24 21'>
+            <Warning />
+          </svg>
+          {error}
+        </div>
+      )}
+    </div>
+  );
+}

c16366152359d93e756786ec24ba0e2be73a127e
Yooodh <eogus2604@hanmail.net>
2025-07-07 00:27:47 +0900

    Feat: 비밀번호 변경 폼 상태 및 유효성 검사 로직 추가

---
+++ digital_game_nomad/app/(auth)/forgot-password/hooks/usePasswordForm.ts
@@ -0,0 +1,91 @@
+// package
+import { useState, useCallback } from 'react';
+
+// slice
+import { FormData, UsePasswordFormProps } from '../types';
+
+export function usePasswordForm({
+  validatePassword,
+  setConfirmPasswordError,
+  setPasswordError,
+  setCurrentPasswordError,
+}: UsePasswordFormProps) {
+  const [formData, setFormData] = useState<FormData>({
+    currentPassword: '',
+    newPassword: '',
+    confirmPassword: '',
+    email: '',
+    emailVerificationCode: '',
+  });
+
+  const [showCurrentPassword, setShowCurrentPassword] =
+    useState<boolean>(false);
+  const [showNewPassword, setShowNewPassword] = useState<boolean>(false);
+  const [showConfirmPassword, setShowConfirmPassword] =
+    useState<boolean>(false);
+
+  const togglePasswordVisibility = useCallback(
+    (field: 'current' | 'new' | 'confirm') => {
+      if (field === 'current') {
+        setShowCurrentPassword((prev) => !prev);
+      } else if (field === 'new') {
+        setShowNewPassword((prev) => !prev);
+      } else if (field === 'confirm') {
+        setShowConfirmPassword((prev) => !prev);
+      }
+    },
+    []
+  );
+
+  const handleInputChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const { name, value } = e.target;
+
+      setFormData((prev) => ({ ...prev, [name]: value }));
+
+      if (name === 'emailVerificationCode') {
+        const numbersOnly = value.replace(/\D/g, '').slice(0, 6);
+        setFormData((prev) => ({ ...prev, [name]: numbersOnly }));
+      } else if (name === 'currentPassword') {
+        setCurrentPasswordError('');
+      } else if (name === 'newPassword') {
+        if (value && !validatePassword(value)) {
+          setPasswordError(
+            '8자 이상, 영문 소문자, 숫자, 특수문자를 포함해주세요'
+          );
+        } else {
+          setPasswordError('');
+        }
+        if (formData.confirmPassword && value !== formData.confirmPassword) {
+          setConfirmPasswordError('비밀번호가 일치하지 않습니다');
+        } else if (formData.confirmPassword) {
+          setConfirmPasswordError('');
+        }
+      } else if (name === 'confirmPassword') {
+        if (value && value !== formData.newPassword) {
+          setConfirmPasswordError('비밀번호가 일치하지 않습니다');
+        } else {
+          setConfirmPasswordError('');
+        }
+      }
+    },
+    [
+      formData.confirmPassword,
+      formData.newPassword,
+      validatePassword,
+      setConfirmPasswordError,
+      setPasswordError,
+      setCurrentPasswordError,
+    ]
+  );
+
+  return {
+    formData,
+    setFormData,
+    handleInputChange,
+    showCurrentPassword,
+    showNewPassword,
+    showConfirmPassword,
+    togglePasswordVisibility,
+  };
+}

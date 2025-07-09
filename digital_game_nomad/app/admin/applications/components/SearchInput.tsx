744d0243939cdc863d20dc436511aa2838798bb1
Yooodh <eogus2604@hanmail.net>
2025-07-10 01:52:09 +0900

    Feat: 검색어 입력 필드 및 검색 아이콘 컴포넌트 구현

---
+++ digital_game_nomad/app/admin/applications/components/SearchInput.tsx
@@ -0,0 +1,29 @@
+// slice
+import styles from '../styles/Applications.module.scss';
+import { SearchInputProps } from '../types';
+
+// layer
+import { Search } from '@/shared/icons';
+
+export default function SearchInput({
+  searchTerm,
+  onSearchChange,
+  placeholder = '검색어를 입력하세요...',
+}: SearchInputProps) {
+  return (
+    <div className={styles.searchContainer}>
+      <div className={styles.boxContainer}>
+        <span className={styles.boxContainer__icon}>
+          <Search />
+        </span>
+        <input
+          type='text'
+          placeholder={placeholder}
+          value={searchTerm}
+          onChange={(e) => onSearchChange(e.target.value)}
+          className={styles.boxContainer__input}
+        />
+      </div>
+    </div>
+  );
+}

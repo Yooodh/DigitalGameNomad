25ac63e939420e72e9c9d0f75656769babc4541f
Yooodh <eogus2604@hanmail.net>
2025-07-10 01:48:31 +0900

    Feat: 신청 카드 헤더 및 선택 체크박스 컴포넌트 구현

---
+++ digital_game_nomad/app/admin/applications/components/AdminApplicationCardHeader.tsx
@@ -0,0 +1,23 @@
+// slice
+import styles from '../styles/Applications.module.scss';
+import { AdminApplicationCardHeaderProps } from '../types';
+
+export default function AdminApplicationCardHeader({
+  application,
+  isSelected,
+  onToggleSelection,
+}: AdminApplicationCardHeaderProps) {
+  return (
+    <div className={styles.gridCardHeaderContainer}>
+      <div className={styles.gridCardHeaderContainer__title}>
+        {application.gameName}
+      </div>
+      <input
+        type='checkbox'
+        checked={isSelected}
+        onChange={() => onToggleSelection(application.id)}
+        className={styles.gridCardHeaderContainer__checkbox}
+      />
+    </div>
+  );
+}

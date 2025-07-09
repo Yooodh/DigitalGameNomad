dd130750b547d88f0e4ebcbce2bca0d52041419e
Yooodh <eogus2604@hanmail.net>
2025-07-10 01:51:35 +0900

    Feat: 날짜/상태 정렬 버튼 컴포넌트 및 활성화 스타일 구현

---
+++ digital_game_nomad/app/admin/applications/components/SortButtons.tsx
@@ -0,0 +1,36 @@
+// slice
+import styles from '../styles/Applications.module.scss';
+import { SortButtonsProps } from '../types';
+
+// layer
+import { Calendar, CheckCircle, ArrowUpDown } from '@/shared/icons';
+
+export default function SortButtons({
+  sortBy,
+  onToggleSort,
+}: SortButtonsProps) {
+  return (
+    <div className={styles.sortContainer}>
+      <button
+        onClick={() => onToggleSort('date')}
+        className={`${styles.sortContainer__btn} ${
+          sortBy === 'date' ? styles.active : ''
+        }`}
+      >
+        <Calendar />
+        날짜
+        <ArrowUpDown />
+      </button>
+      <button
+        onClick={() => onToggleSort('status')}
+        className={`${styles.sortContainer__btn} ${
+          sortBy === 'status' ? styles.active : ''
+        }`}
+      >
+        <CheckCircle />
+        상태
+        <ArrowUpDown />
+      </button>
+    </div>
+  );
+}

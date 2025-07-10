3eaed78c7cc3cad53d2d071c035bd04630e75897
Yooodh <eogus2604@hanmail.net>
2025-07-11 02:10:43 +0900

    Feat: 참여 신청 내역 관리자 페이지 추가

---
+++ digital_game_nomad/app/admin/applications/page.tsx
@@ -0,0 +1,10 @@
+// slice
+import AdminApplicationsContainer from './containers/Applications.container';
+
+export default function page() {
+  return (
+    <div>
+      <AdminApplicationsContainer />
+    </div>
+  );
+}

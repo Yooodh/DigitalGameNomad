4100b2282487c0b8c2c4c36b9fd18ceaa90947d9
Yooodh <eogus2604@hanmail.net>
2025-07-15 03:34:07 +0900

    Feat: 문의 내역 없음 안내 메시지 표시 컴포넌트 구현

---
+++ digital_game_nomad/app/support/inquiry/detail/components/EmptyState.tsx
@@ -0,0 +1,16 @@
+// slice
+import styles from '../styles/Detail.module.scss';
+
+// layer
+import { Alert } from '@/shared/icons';
+
+export default function EmptyState() {
+  return (
+    <div className={styles.emptyContainer}>
+      <div className={styles.emptyContainer__icon}>
+        <Alert />
+      </div>
+      <h3 className={styles.emptyContainer__title}>문의 내역이 없습니다</h3>
+    </div>
+  );
+}

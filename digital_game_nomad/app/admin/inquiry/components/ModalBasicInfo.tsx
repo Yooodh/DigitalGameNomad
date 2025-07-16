0a0d591766bdd32c3d8be7a103baaa8ecfa233b5
Yooodh <eogus2604@hanmail.net>
2025-07-17 04:07:30 +0900

    Feat: 모달 기본 정보 섹션 컴포넌트 구현

---
+++ digital_game_nomad/app/admin/inquiry/components/ModalBasicInfo.tsx
@@ -0,0 +1,34 @@
+// slice
+import styles from '../styles/Inquiry.module.scss';
+import { ModalBasicInfoProps } from '../types';
+
+export default function ModalBasicInfo({
+  selectedInquiry,
+  getStatusClass,
+}: ModalBasicInfoProps) {
+  return (
+    <div className={styles.detailSectionContainer}>
+      <h3>기본 정보</h3>
+      <div className={styles.gridContainer}>
+        <div className={styles.detailContainer}>
+          <label>문의번호:</label>
+          <span>{selectedInquiry.id}</span>
+        </div>
+        <div className={styles.detailContainer}>
+          <label>등록일시:</label>
+          <span>{selectedInquiry.createdAt}</span>
+        </div>
+        <div className={styles.detailContainer}>
+          <label>상태:</label>
+          <div
+            className={`${styles.statusBadge} ${
+              styles[getStatusClass(selectedInquiry.status)]
+            }`}
+          >
+            {selectedInquiry.status}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}

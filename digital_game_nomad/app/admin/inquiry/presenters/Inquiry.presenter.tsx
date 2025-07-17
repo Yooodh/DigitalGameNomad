5974d0e1d4b14e3c5d75cb2d24ab4e6b8de8153e
Yooodh <eogus2604@hanmail.net>
2025-07-18 05:29:07 +0900

    Feat: 문의 내역 관리자 페이지 프레젠터 컴포넌트 구현

---
+++ digital_game_nomad/app/admin/inquiry/presenters/Inquiry.presenter.tsx
@@ -0,0 +1,81 @@
+// slice
+import Stats from '../components/Stats';
+import Filters from '../components/Filters';
+import Table from '../components/Table';
+import Pagination from '../components/Pagination';
+import Modal from '../components/Modal';
+import styles from '../styles/Inquiry.module.scss';
+import { AdminInquiryPresenterProps } from '../types';
+
+export default function AdminInquiryPresenter({
+  inquiries,
+  filteredInquiries,
+  currentInquiries,
+  selectedInquiry,
+  statusFilter,
+  searchTerm,
+  currentPage,
+  totalPages,
+  isDetailModalOpen,
+  isReplyMode,
+  replyContent,
+  ITEMS_PER_PAGE,
+  MAX_VISIBLE_PAGES,
+  setSearchTerm,
+  setStatusFilter,
+  setCurrentPage,
+  setReplyContent,
+  updateInquiryStatus,
+  saveReply,
+  toggleReplyMode,
+  cancelReply,
+  openDetailModal,
+  closeDetailModal,
+  getStatusClass,
+}: AdminInquiryPresenterProps) {
+  return (
+    <div className={styles.container}>
+      <Stats inquiries={inquiries} />
+
+      <Filters
+        searchTerm={searchTerm}
+        setSearchTerm={setSearchTerm}
+        statusFilter={statusFilter}
+        setStatusFilter={setStatusFilter}
+      />
+
+      <div className={styles.sectionContainer}>
+        <Table
+          currentInquiries={currentInquiries}
+          openDetailModal={openDetailModal}
+          updateInquiryStatus={updateInquiryStatus}
+          getStatusClass={getStatusClass}
+        />
+        <Pagination
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+          totalPages={totalPages}
+          MAX_VISIBLE_PAGES={MAX_VISIBLE_PAGES}
+          filteredInquiriesLength={filteredInquiries.length}
+          currentInquiriesLength={currentInquiries.length}
+          ITEMS_PER_PAGE={ITEMS_PER_PAGE}
+        />
+      </div>
+
+      {isDetailModalOpen && selectedInquiry && (
+        <Modal
+          selectedInquiry={selectedInquiry}
+          isReplyMode={isReplyMode}
+          replyContent={replyContent}
+          closeDetailModal={closeDetailModal}
+          updateInquiryStatus={updateInquiryStatus}
+          setReplyContent={setReplyContent}
+          saveReply={saveReply}
+          toggleReplyMode={toggleReplyMode}
+          cancelReply={cancelReply}
+          getStatusClass={getStatusClass}
+        />
+      )}
+    </div>
+  );
+}

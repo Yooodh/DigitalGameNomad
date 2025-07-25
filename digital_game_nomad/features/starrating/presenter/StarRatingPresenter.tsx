a05dc18591a8c272137be6ce1d6e78c724b46dba
Yooodh <eogus2604@hanmail.net>
2025-07-26 01:27:36 +0900

    Refactor: 인터랙티브 props 기반 이벤트 핸들러 및 시각적 피드백을 동적으로 기능 확장 및 변경

--- digital_game_nomad/features/starrating/presenter/StarRatingPresenter.tsx
+++ digital_game_nomad/features/starrating/presenter/StarRatingPresenter.tsx
@@ -5,10 +5,32 @@ import { StarRatingPresenterProps } from '../types/StarRating.types';
 export default function StarRatingPresenter({
   processedStars,
   rating,
-  showRatingText,
+  showRatingText = false,
+  size = 'medium',
+  onStarClick,
+  onStarMouseEnter,
+  onStarMouseLeave,
+  hoverRating,
+  isInteractive = false,
 }: StarRatingPresenterProps) {
+  const sizeClass = {
+    small: styles.starSmall,
+    medium: styles.starMedium,
+    large: styles.starLarge,
+  }[size];
+
+  const starRatingClasses = `${styles.starRating} ${sizeClass} ${
+    isInteractive ? styles.isInteractive : ''
+  }`;
+
+  const displayTextRating = showRatingText
+    ? hoverRating && hoverRating > 0
+      ? hoverRating
+      : rating
+    : rating;
+
   return (
-    <div className={styles.starRating}>
+    <div className={starRatingClasses}>
       {processedStars.map((star) => (
         <span
           key={star.key}
@@ -16,13 +38,20 @@ export default function StarRatingPresenter({
           style={
             { '--percentage': `${star.percentage}%` } as React.CSSProperties
           }
+          onClick={() => isInteractive && onStarClick && onStarClick(star.key)}
+          onMouseEnter={() =>
+            isInteractive && onStarMouseEnter && onStarMouseEnter(star.key)
+          }
+          onMouseLeave={() =>
+            isInteractive && onStarMouseLeave && onStarMouseLeave()
+          }
         >
           ★
         </span>
       ))}
       {showRatingText && (
         <span className={styles.starContainer__text}>
-          ({rating.toFixed(1)}/5)
+          ({displayTextRating.toFixed(0)}/5)
         </span>
       )}
     </div>

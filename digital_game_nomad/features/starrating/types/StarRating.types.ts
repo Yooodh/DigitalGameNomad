aeb1f07dcb62cd1e330cf2f35fdff686c37aac70
Yooodh <eogus2604@hanmail.net>
2025-07-26 01:21:06 +0900

    Feat: 별점 기능 관련 타입 정의 확장 및 구조화

--- digital_game_nomad/features/starrating/types/StarRating.types.ts
+++ digital_game_nomad/features/starrating/types/StarRating.types.ts
@@ -1,11 +1,42 @@
-export type StarRatingProps = {
+import { STAR_SIZES } from '../constants/StarRating.constants';
+
+export type StarSize = (typeof STAR_SIZES)[keyof typeof STAR_SIZES];
+
+export type StarRatingDisplayProps = {
   rating: number;
   maxRating?: number;
   showRatingText?: boolean;
+  size?: StarSize;
 };
 
 export type StarRatingPresenterProps = {
   processedStars: { percentage: number; key: number }[];
   rating: number;
   showRatingText: boolean;
+  size?: StarSize;
+  onStarClick?: (starIndex: number) => void;
+  onStarMouseEnter?: (starIndex: number) => void;
+  onStarMouseLeave?: () => void;
+  hoverRating?: number;
+  isInteractive?: boolean;
 };
+
+export type ProcessedStar = {
+  key: number;
+  percentage: number;
+};
+
+export interface UseInteractiveStarRatingProps {
+  initialRating: number;
+  maxRating?: number;
+  onRatingChange: (rating: number) => void;
+}
+
+export interface UseInteractiveStarRatingReturn {
+  currentRating: number;
+  hoverRating: number;
+  processedStars: ProcessedStar[];
+  handleStarClick: (starIndex: number) => void;
+  handleStarMouseEnter: (starIndex: number) => void;
+  handleStarMouseLeave: () => void;
+}

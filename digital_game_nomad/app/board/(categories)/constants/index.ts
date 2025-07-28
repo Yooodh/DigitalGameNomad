cfbb35ef6d5d1ce3221f470107aba7f675a692e1
Yooodh <eogus2604@hanmail.net>
2025-07-29 04:08:01 +0900

    Feat: 자유 및 후기 게시판 상수 파일 추가

---
+++ digital_game_nomad/app/board/(categories)/constants/index.ts
@@ -0,0 +1,5 @@
+export const POSTS_PER_PAGE = 25;
+export const MAX_VISIBLE_PAGES = 5;
+export const FREE_BOARD_TOPIC = '자유';
+export const REVIEW_BOARD_TOPIC = '후기';
+export const INITIAL_SEARCH_FILTER_OPTION = '제목';

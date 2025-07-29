3728a5212ea06f93263c31b6f2bcc351ea525946
Yooodh <eogus2604@hanmail.net>
2025-07-30 04:32:52 +0900

    Feat: 자유 게시판 타입 정의

---
+++ digital_game_nomad/app/board/(categories)/free/types/index.ts
@@ -0,0 +1,28 @@
+import { ChangeEvent } from 'react';
+import { PostData } from '../../../types';
+
+export type TableProps = {
+  currentPosts: PostData[];
+  filteredPostsCount: number;
+  startIndex: number;
+  onPostClick: (postId: string) => void;
+};
+
+export type FreePresenterProps = {
+  currentPosts: PostData[];
+  filteredPostsCount: number;
+  startIndex: number;
+  totalPages: number;
+  currentPage: number;
+  filterOption: string;
+  inputValue: string;
+  onPostClick: (postId: string) => void;
+  onWriteClick: () => void;
+  onListClick: () => void;
+  onFilterChange: (e: ChangeEvent<HTMLSelectElement>) => void;
+  onInputChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  onSearch: () => void;
+  onPageChange: (page: number) => void;
+  postsPerPage: number;
+  maxVisiblePages: number;
+};

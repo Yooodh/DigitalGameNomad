6be817af8e78c2919b3192d003efce83b011ded9
Yooodh <eogus2604@hanmail.net>
2025-08-01 03:43:12 +0900

    Feat: 댓글 폼 및 리스트 통합 컴포넌트 구현

---
+++ digital_game_nomad/app/board/(posts)/detail/components/CommentSection.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+// slice
+import CommentForm from './CommentForm';
+import CommentList from './CommentList';
+import styles from '../styles/Detail.module.scss';
+import { CommentSectionProps } from '../types';
+
+export default function CommentSection({
+  currentPostComments,
+  newComment,
+  setNewComment,
+  handleCommentSubmit,
+  loggedInUserKey,
+  editingCommentId,
+  editedCommentContent,
+  setEditedCommentContent,
+  handleDeleteComment,
+  handleEditCommentClick,
+  handleSaveEditedComment,
+  handleCancelEditComment,
+}: CommentSectionProps) {
+  return (
+    <div className={styles.commentContainer}>
+      <h3 className={styles.titleContainer}>
+        댓글{' '}
+        <span className={styles.titleContainer__count}>
+          ({currentPostComments.length})
+        </span>
+      </h3>
+
+      <CommentForm
+        newComment={newComment}
+        setNewComment={setNewComment}
+        handleCommentSubmit={handleCommentSubmit}
+      />
+
+      <CommentList
+        comments={currentPostComments}
+        loggedInUserKey={loggedInUserKey}
+        editingCommentId={editingCommentId}
+        editedCommentContent={editedCommentContent}
+        setEditedCommentContent={setEditedCommentContent}
+        handleDeleteComment={handleDeleteComment}
+        handleEditCommentClick={handleEditCommentClick}
+        handleSaveEditedComment={handleSaveEditedComment}
+        handleCancelEditComment={handleCancelEditComment}
+      />
+    </div>
+  );
+}

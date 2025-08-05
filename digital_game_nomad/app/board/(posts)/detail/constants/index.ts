df967ef38976bfa210134a17c3a3a641b0a7af7d
Yooodh <eogus2604@hanmail.net>
2025-08-05 13:23:21 +0900

    Feat: 게시판 상세 페이지 상수 파일 추가

---
+++ digital_game_nomad/app/board/(posts)/detail/constants/index.ts
@@ -0,0 +1,24 @@
+export const MOCK_LOGGED_IN_USER_KEY = 999;
+
+export const BOARD_TYPES = {
+  FREE: '자유',
+  REVIEW: '후기',
+};
+
+export const BOARD_PATHS = {
+  FREE: '/board/free',
+  REVIEW: '/board/review',
+};
+
+export const MESSAGES = {
+  LOADING_POST: '게시글을 불러오는 중...',
+  EDIT_ERROR_NO_POST: '수정할 게시글 데이터를 찾을 수 없습니다.',
+  EDIT_ERROR_MESSAGE: '게시글 정보를 불러오지 못했습니다.',
+  NOT_FOUND_TITLE: '게시글을 찾을 수 없습니다.',
+  NOT_FOUND_MESSAGE: '존재하지 않거나 삭제된 게시글입니다.',
+};
+
+export const BUTTON_TEXT = {
+  GO_TO_LIST: '목록으로 돌아가기',
+  GO_TO_LIST_SHORT: '목록으로',
+};

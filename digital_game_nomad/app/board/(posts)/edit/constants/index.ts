9657ce90746a499dbdd5496edb0868275c1d850e
Yooodh <eogus2604@hanmail.net>
2025-08-07 05:58:53 +0900

    Feat: 게시글 수정 페이지 상수 파일 추가

---
+++ digital_game_nomad/app/board/(posts)/edit/constants/index.ts
@@ -0,0 +1,14 @@
+export const MESSAGES = {
+  LOADING_POST: '게시글을 불러오는 중...',
+  POST_NOT_FOUND_TITLE: '수정할 게시글을 찾을 수 없습니다.',
+  POST_NOT_FOUND_MESSAGE: 'URL이 올바른지 확인해주세요.',
+};
+
+export const BUTTON_TEXT = {
+  GO_BACK: '돌아가기',
+};
+
+export const REQUIREMENT_TEXT = {
+  REQUIRED: '(필수)',
+  OPTIONAL: '(선택사항)',
+};

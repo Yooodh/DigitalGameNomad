e78e2d4b6ed14628d82a05d9fc5043b43a8ecf75
Yooodh <eogus2604@hanmail.net>
2025-08-22 04:05:31 +0900

    Refactor: 가독성 및 일관성 유지를 위해 PascalCase 규칙에 맞게 파일명 변경

--- digital_game_nomad/app/(auth)/forgot-password/presenter/ForgotPasswordPresenter.tsx
+++ digital_game_nomad/app/(auth)/forgot-password/presenter/ForgotPasswordPresenter.tsx
@@ -1,7 +1,7 @@
 // slice
 import FirstStepEmailVerification from '../components/FirstStepEmailVerification';
 import SecondStepNewPassword from '../components/SecondStepNewPassword';
-import ThridStepCompletion from '../components/ThridStepCompletion';
+import ThirdStepCompletion from '../components/ThirdStepCompletion';
 import styles from '../styles/ForgotPassword.module.scss';
 import { ForgotPasswordPresenterProps } from '../types';
 
@@ -11,14 +11,13 @@ import { ProgressBar } from '@/features/progressbar';
 export default function ForgotPasswordPresenter(
   props: ForgotPasswordPresenterProps
 ) {
-  const { step, handleSubmit, handlePrev } = props;
+  const { step, handleSubmit, handlePrev, resetForm } = props;
 
   return (
     <div className={styles.resetPasswordContainer}>
       <div className={styles.cardContainer}>
         <div className={styles.headerContainer}>
           <h1 className={styles.headerContainer__title}>비밀번호 찾기</h1>
-
           <ProgressBar currentStep={step} totalSteps={3} />
         </div>
         <form onSubmit={handleSubmit} className={styles.form}>
@@ -26,7 +25,7 @@ export default function ForgotPasswordPresenter(
           {step === 2 && (
             <SecondStepNewPassword {...props} handlePrev={handlePrev} />
           )}
-          {step === 3 && <ThridStepCompletion />}
+          {step === 3 && <ThirdStepCompletion resetForm={resetForm} />}{' '}
         </form>
       </div>
     </div>

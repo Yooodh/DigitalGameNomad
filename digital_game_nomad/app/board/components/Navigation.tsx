9399e677d59a7a16f79b5c051f399c3863fbb072
Yooodh <eogus2604@hanmail.net>
2025-09-05 05:05:54 +0900

    Refactor: TabType을 ActiveTab으로 타입 교체

--- digital_game_nomad/app/board/components/Navigation.tsx
+++ digital_game_nomad/app/board/components/Navigation.tsx
@@ -1,9 +1,9 @@
 // slice
 import styles from '../styles/Board.module.scss';
-import { NavigationProps, TabType } from '../types';
+import { NavigationProps, ActiveTab } from '../types';
 
 export default function Navigation({ activeTab, onTabClick }: NavigationProps) {
-  const tabs: TabType[] = ['전체', '자유', '후기'];
+  const tabs: ActiveTab[] = ['전체', '자유', '후기'];
   return (
     <nav className={styles.navContainer}>
       <div className={styles.navContainer__nav}>

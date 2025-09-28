2a7dc515ede359e828f3e407a72b8822d474c475
Yooodh <eogus2604@hanmail.net>
2025-09-29 03:03:01 +0900

    Feat: 안내 문구 구조 및 스타일 분리

--- digital_game_nomad/app/apply/components/Footer.tsx
+++ digital_game_nomad/app/apply/components/Footer.tsx
@@ -4,7 +4,12 @@ import styles from '../styles/Apply.module.scss';
 export default function Footer() {
   return (
     <div className={styles.footerContainer}>
-      <p>신청서 제출 후 검토를 거쳐 승인 여부를 안내드립니다.</p>
+      <p className={styles.footerContainer__notify}>
+        참여 신청 완료 시 기업 등급으로 변경됩니다.
+      </p>
+      <p className={styles.footerContainer__guide}>
+        신청서 제출 후 검토를 거쳐 승인 여부를 안내드립니다.
+      </p>
     </div>
   );
 }

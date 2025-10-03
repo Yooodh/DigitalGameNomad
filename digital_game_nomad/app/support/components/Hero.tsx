7abfb5a092d7537504981dead1337da6723160f1
Yooodh <eogus2604@hanmail.net>
2025-10-04 02:20:22 +0900

    Chore: 안내 문구 문장부호 수정

--- digital_game_nomad/app/support/components/Hero.tsx
+++ digital_game_nomad/app/support/components/Hero.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
       <div className={styles.heroContainer__hero}>
         <h1 className={styles.heroContainer__title}>고객센터</h1>
         <p className={styles.heroContainer__subTitle}>
-          궁금한 점이 있으시면 언제든지 문의해주세요
+          궁금한 점이 있으시면 언제든지 문의해주세요.
         </p>
       </div>
     </div>

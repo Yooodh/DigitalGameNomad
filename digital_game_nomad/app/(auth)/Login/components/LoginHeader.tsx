1e4ea32bd9202764999cc96583075418ff4a38cf
Yooodh <eogus2604@hanmail.net>
2025-10-09 02:50:43 +0900

    Chore: 안내 문구 문장부호 수정

--- digital_game_nomad/app/(auth)/Login/components/LoginHeader.tsx
+++ digital_game_nomad/app/(auth)/Login/components/LoginHeader.tsx
@@ -5,7 +5,7 @@ export default function LoginHeader() {
   return (
     <div className={styles.headerContainer}>
       <h1 className={styles.headerContainer__title}>환영합니다</h1>
-      <p className={styles.headerContainer__subtitle}>계정에 로그인하세요</p>
+      <p className={styles.headerContainer__subtitle}>계정에 로그인하세요.</p>
     </div>
   );
 }

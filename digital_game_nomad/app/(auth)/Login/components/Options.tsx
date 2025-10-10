88a84a49db7117d374ee1389f3c8076b1130d0f7
Yo_o. <eogus2604@hanmail.net>
2025-10-11 05:17:31 +0900

    Style: 로그인 페이지 반응형 스타일 개선 (#123)
    * Chore: 안내 문구 문장부호 수정
    
    * Refactor: 로그인 상태 유지 체크박스 제거
    
    * Style: 로그인 페이지 반응형 스타일 개선

--- digital_game_nomad/app/(auth)/Login/components/Options.tsx
+++ digital_game_nomad/app/(auth)/Login/components/Options.tsx
@@ -5,12 +5,6 @@ import styles from '../styles/Login.module.scss';
 export default function Options() {
   return (
     <div className={styles.optionContainer}>
-      <label className={styles.optionContainer__checkboxWrap}>
-        <input type='checkbox' className={styles.optionContainer__checkbox} />
-        <span className={styles.optionContainer__checkboxTxt}>
-          로그인 상태 유지
-        </span>
-      </label>
       <Link href='/forgot-password'>
         <p className={styles.optionContainer__forgotPassword}>
           비밀번호를 잊으셨나요?

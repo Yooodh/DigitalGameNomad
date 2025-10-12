c9487f052773d9e500151e56e1eedf7d4336a88a
Yo_o. <eogus2604@hanmail.net>
2025-10-13 02:57:48 +0900

    Style: 기업 참여 신청서 작성 페이지 반응형 스타일 개선 (#125)
    * Chore: 안내 문구 문장부호 수정
    
    * Style: 기업 참여 신청서 작성 페이지 반응형 스타일 개선

--- digital_game_nomad/app/apply/components/Header.tsx
+++ digital_game_nomad/app/apply/components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
     <div className={styles.headerContainer}>
       <h1 className={styles.headerContainer__title}>참여신청</h1>
       <p className={styles.headerContainer__subTitle}>
-        게임 전시회에 참여하기 위한 정보를 입력해주세요
+        게임 전시회에 참여하기 위한 정보를 입력해주세요.
       </p>
     </div>
   );

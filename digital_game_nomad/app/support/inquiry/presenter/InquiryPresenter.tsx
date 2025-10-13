1536a6294e3a47cf7bec7722fb62999446210a63
Yo_o. <eogus2604@hanmail.net>
2025-10-14 02:37:40 +0900

    Style: 문의 페이지 반응형 스타일 개선 (#126)
    * Style: 버튼 로딩 상태 시 텍스트 위치 변경
    
    * Chore: 안내 문구 문장부호 수정
    
    * Style: 문의 페이지 반응형 스타일 개선

--- digital_game_nomad/app/support/inquiry/presenter/InquiryPresenter.tsx
+++ digital_game_nomad/app/support/inquiry/presenter/InquiryPresenter.tsx
@@ -17,7 +17,7 @@ export default function InquiryPresenter({
     <div className={styles.inquiryContainer}>
       <Header
         title='문의하기'
-        subTitle='궁금한 점이 있으시면 언제든 문의해 주세요'
+        subTitle='궁금한 점이 있으시면 언제든 문의해 주세요.'
       />
 
       <div className={styles.cardContainer}>
@@ -28,7 +28,7 @@ export default function InquiryPresenter({
           <FormInput
             id='title'
             label='제목'
-            placeholder='제목을 입력해주세요'
+            placeholder='제목을 입력해주세요.'
             value={formData.title}
             onChange={(value) => handleInputChange('title', value)}
             error={errors.title}
@@ -39,7 +39,7 @@ export default function InquiryPresenter({
           <FormTextarea
             id='content'
             label='내용'
-            placeholder='문의 내용을 입력해주세요'
+            placeholder='문의 내용을 입력해주세요.'
             value={formData.text}
             onChange={(value) => handleInputChange('text', value)}
             error={errors.text}

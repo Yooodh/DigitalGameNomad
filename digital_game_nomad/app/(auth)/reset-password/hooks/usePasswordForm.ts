278b1006c8261d2075ef72c50492d685c97ffabd
Yo_o. <eogus2604@hanmail.net>
2025-11-05 03:01:27 +0900

    Feat: 비밀번호 변경 페이지 폼 상태 초기화 기능 추가 및 스타일 개선 (#148)
    * Feat: 컴포넌트 마운트 시 폼 상태 초기화 기능 추가
    
    * Chore: 안내 문구 문장부호 수정
    
    * Style: 스타일 개선 및 버튼 인터랙션 효과 수정

--- digital_game_nomad/app/(auth)/reset-password/hooks/usePasswordForm.ts
+++ digital_game_nomad/app/(auth)/reset-password/hooks/usePasswordForm.ts
@@ -21,19 +21,19 @@ export function usePasswordForm({ validatePassword }: UsePasswordFormProps) {
         if (value && !validatePassword(value)) {
           setError(
             'newPasswordError',
-            '8자 이상, 영문 소문자, 숫자, 특수문자를 포함해주세요'
+            '8자 이상, 영문 소문자, 숫자, 특수문자를 포함해주세요.'
           );
         } else {
           setError('newPasswordError', '');
         }
         if (formData.confirmPassword && value !== formData.confirmPassword) {
-          setError('confirmPasswordError', '비밀번호가 일치하지 않습니다');
+          setError('confirmPasswordError', '비밀번호가 일치하지 않습니다.');
         } else if (formData.confirmPassword) {
           setError('confirmPasswordError', '');
         }
       } else if (name === 'confirmPassword') {
         if (value && value !== formData.newPassword) {
-          setError('confirmPasswordError', '비밀번호가 일치하지 않습니다');
+          setError('confirmPasswordError', '비밀번호가 일치하지 않습니다.');
         } else {
           setError('confirmPasswordError', '');
         }

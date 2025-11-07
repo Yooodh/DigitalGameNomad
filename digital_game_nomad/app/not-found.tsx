d426103e4b60badb2d406513c2f5baa8b44925d1
Yooodh <eogus2604@hanmail.net>
2025-11-08 01:22:27 +0900

    Feat: 404 접근 제한 페이지 구현

---
+++ digital_game_nomad/app/not-found.tsx
@@ -0,0 +1,21 @@
+// package
+import Image from 'next/image';
+
+// layer
+import styles from '@/shared/ui/forbidden/View.module.scss';
+
+export default function NotFound() {
+  return (
+    <div className={styles.Container}>
+      <Image
+        src='/images/logo_white_half.png'
+        alt='logo'
+        width={200}
+        height={200}
+      />
+      <h1>404</h1>
+      <p>페이지를 찾을 수 없습니다.</p>
+      <a href='/'>홈으로 이동</a>
+    </div>
+  );
+}

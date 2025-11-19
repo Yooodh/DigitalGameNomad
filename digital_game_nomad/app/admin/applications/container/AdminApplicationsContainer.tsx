6dcef27881d5fbb8f6da661786eb0e62feab4841
Yooodh <eogus2604@hanmail.net>
2025-11-20 04:30:17 +0900

    Feat: 참여 신청 내역 관리자 페이지 로딩 컴포넌트 적용

--- digital_game_nomad/app/admin/applications/container/AdminApplicationsContainer.tsx
+++ digital_game_nomad/app/admin/applications/container/AdminApplicationsContainer.tsx
@@ -4,6 +4,9 @@
 import AdminApplicationsPresenter from '../presenter/AdminApplicationsPresenter';
 import { useAdminApplications } from '../hooks/useAdminApplications';
 
+// layer
+import { Loading } from '@/features/loading';
+
 export default function AdminApplicationsContainer() {
   const {
     applications,
@@ -27,25 +30,27 @@ export default function AdminApplicationsContainer() {
   } = useAdminApplications();
 
   return (
-    <AdminApplicationsPresenter
-      applications={applications}
-      searchTerm={searchTerm}
-      statusFilter={statusFilter}
-      sortBy={sortBy}
-      sortOrder={sortOrder}
-      filteredApplications={filteredApplications}
-      selectedApplications={selectedApplications}
-      showBulkActions={showBulkActions}
-      onSearchChange={onSearchChange}
-      onStatusFilterChange={onStatusFilterChange}
-      onResetFilters={onResetFilters}
-      onToggleSort={onToggleSort}
-      onToggleApplicationSelection={onToggleApplicationSelection}
-      onSelectAllApplications={onSelectAllApplications}
-      onBulkStatusChange={onBulkStatusChange}
-      onBulkDelete={onBulkDelete}
-      onChangeStatus={onChangeStatus}
-      getStatusCount={getStatusCount}
-    />
+    <Loading message='신청 내역 불러오는 중...'>
+      <AdminApplicationsPresenter
+        applications={applications}
+        searchTerm={searchTerm}
+        statusFilter={statusFilter}
+        sortBy={sortBy}
+        sortOrder={sortOrder}
+        filteredApplications={filteredApplications}
+        selectedApplications={selectedApplications}
+        showBulkActions={showBulkActions}
+        onSearchChange={onSearchChange}
+        onStatusFilterChange={onStatusFilterChange}
+        onResetFilters={onResetFilters}
+        onToggleSort={onToggleSort}
+        onToggleApplicationSelection={onToggleApplicationSelection}
+        onSelectAllApplications={onSelectAllApplications}
+        onBulkStatusChange={onBulkStatusChange}
+        onBulkDelete={onBulkDelete}
+        onChangeStatus={onChangeStatus}
+        getStatusCount={getStatusCount}
+      />
+    </Loading>
   );
 }

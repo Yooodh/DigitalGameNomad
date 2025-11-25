c4bf252ce20940d5070f400557ea4c67fef14b60
Yo_o. <eogus2604@hanmail.net>
2025-11-26 03:47:25 +0900

    Style: 게시판 상세/작성/수정 페이지 스타일 인터랙션 개선  (#169)
    * Chore: 안내 문구 문장부호 수정
    
    * Style: 게시글 상세 페이지 버튼 스타일 개선
    
    * Style: 업로드 영역 인터랙션 개선

--- digital_game_nomad/app/board/(posts)/edit/presenter/EditPresenter.tsx
+++ digital_game_nomad/app/board/(posts)/edit/presenter/EditPresenter.tsx
@@ -37,7 +37,7 @@ export default function EditPresenter({
           required
           InputComponent='input'
           otherProps={{
-            placeholder: '제목을 입력하세요',
+            placeholder: '제목을 입력하세요.',
             maxLength: 50,
           }}
         />
@@ -49,7 +49,7 @@ export default function EditPresenter({
           required
           InputComponent='textarea'
           otherProps={{
-            placeholder: '내용을 입력하세요',
+            placeholder: '내용을 입력하세요.',
             rows: 15,
           }}
         />

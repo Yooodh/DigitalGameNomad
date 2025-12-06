e7a4a271a1c99c44cff656e8f14781db8c63fbed
Yooodh <eogus2604@hanmail.net>
2025-12-07 01:08:25 +0900

    Refactor: alert 알림 방식 toast로 변경

--- digital_game_nomad/app/apply/hooks/useFormSubmission.ts
+++ digital_game_nomad/app/apply/hooks/useFormSubmission.ts
@@ -1,6 +1,7 @@
 // package
 import { useState, useCallback } from 'react';
 import { useRouter } from 'next/navigation';
+import { toast } from 'react-toastify';
 
 // slice
 import { UseFormSubmissionProps } from '../types';
@@ -28,11 +29,11 @@ export function useFormSubmission<T>({
         if (onSubmitSuccess) {
           await onSubmitSuccess(dataToSubmit);
         }
-        alert('신청서가 성공적으로 제출되었습니다.');
+        toast.success('신청서가 성공적으로 제출되었습니다.');
         router.push('/participation');
       } catch (error) {
         console.error('제출 중 오류가 발생했습니다:', error);
-        alert('신청서 제출 중 오류가 발생했습니다. 다시 시도해 주세요.');
+        toast.error('신청서 제출 중 오류가 발생했습니다. 다시 시도해 주세요.');
       } finally {
         setIsSubmitting(false);
       }

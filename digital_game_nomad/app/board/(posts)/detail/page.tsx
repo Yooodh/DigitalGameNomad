fc77a2ac19b59b94206a1e2b4c44c6152cbd64e6
Yooodh <eogus2604@hanmail.net>
2026-01-04 01:20:21 +0900

    Feat: Suspense로 감싸 로딩 상태 처리 추가

--- digital_game_nomad/app/board/(posts)/detail/page.tsx
+++ digital_game_nomad/app/board/(posts)/detail/page.tsx
@@ -1,6 +1,16 @@
+// package
+import { Suspense } from 'react';
+
 // slice
 import { Detail } from '.';
 
+// layer
+import Spinner from '@/shared/components/Spinner';
+
 export default function Page() {
-  return <Detail />;
+  return (
+    <Suspense fallback={<Spinner message='게시글 불러오는 중...' />}>
+      <Detail />
+    </Suspense>
+  );
 }

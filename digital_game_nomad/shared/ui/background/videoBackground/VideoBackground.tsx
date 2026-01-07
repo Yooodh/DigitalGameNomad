4fe1be6bd3caf987d4fd31deb5dd34b97b323a68
Yooodh <eogus2604@hanmail.net>
2026-01-08 03:32:44 +0900

    Fix: 모바일 브라우저 비디오 배경 재생 및 노출 문제 수정

--- digital_game_nomad/shared/ui/background/videoBackground/VideoBackground.tsx
+++ digital_game_nomad/shared/ui/background/videoBackground/VideoBackground.tsx
@@ -3,7 +3,20 @@ import styles from './VidoeBackground.module.scss';
 
 export default function Video() {
   return (
-    <video className={styles.video} autoPlay muted loop>
+    <video
+      className={styles.video}
+      autoPlay
+      muted={true}
+      loop
+      playsInline
+      controls={false}
+      disablePictureInPicture
+      aria-hidden='true'
+      webkit-playsinline='true'
+      x5-playsinline='true'
+      preload='auto'
+      poster='/images/network.jpg'
+    >
       <source src='/videos/network.mp4' type='video/mp4' />
     </video>
   );

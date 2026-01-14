7e6238cc327311ad0e6fc4e9d1013de390d2b986
Yo_o. <eogus2604@hanmail.net>
2026-01-14 21:39:58 +0900

    Chore: 비로그인 전용 페이지 안내 문구 수정 (#217)

--- digital_game_nomad/app/(unauth)/register/hooks/useEmailRegistration.ts
+++ digital_game_nomad/app/(unauth)/register/hooks/useEmailRegistration.ts
@@ -62,7 +62,7 @@ export const useEmailRegistration = ({
 
   const checkEmailDuplicate = useCallback(async () => {
     if (!isValidEmailFormat(fullEmailForValidation)) {
-      toast.warning('올바른 이메일 주소 형식을 입력하고 중복확인을 해주세요.');
+      toast.warning('올바른 이메일 주소 형식을 입력하고 중복 확인을 해주세요.');
       setValidation((prev: ValidationState) => ({ ...prev, email: false }));
       return;
     }
